7f56a1347981843b31236f13822724bc1a6ec9ca
lipovnat000 <lipovnat000@gmail.com>
2024-06-06 01:17:03 -0600

    Learning04

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,20 @@
+class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        this._studentName = studentName;
+        this._topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"{this._studentName} - {this._topic}";
+    }
+
+    public string GetStudentName() {
+        return this._studentName;
+    }
+}
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,13 @@
+class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic) {
+        this._title = title;
+    }
+    public string GetWritingInformation() {
+        string studentName = GetStudentName();
+        
+        return $"{_title} by {studentName}";
+    }
+}
