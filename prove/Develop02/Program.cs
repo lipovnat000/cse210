f9cd3dbd6d53edd3cd11aa866cb0bc1277cac8d3
lipovnat000 <lipovnat000@gmail.com>
2024-06-20 14:56:36 -0600

    Updated Develop02

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -7,7 +7,7 @@ class Program
     static void Main(string[] args)
     {
         Journal journal = new Journal();
-        string entry;
+        string entry = "";
         string filename = "";
 
         int choice = 0;
@@ -19,20 +19,23 @@ class Program
     4. Save
     5. Quit");
 
-        while (choice != 5) {
+        do {
             Console.Write("What would you like to do? ");
             choice = int.Parse(Console.ReadLine());
 
             if (choice == 1) {
-
+                entry = Console.ReadLine();
                 if (string.IsNullOrWhiteSpace(entry)) {
                     break;
                 } else {
                     journal.AddEntry(entry);
                 }
-            } else if (choice == 2) {
+            } 
+            else if (choice == 2) {
                 journal.DisplayAll(filename);
+            } else if (choice == 4) {
+                journal.SaveToFile(filename);
             }
-        }
+       }while (choice != 5);
     }
 }
